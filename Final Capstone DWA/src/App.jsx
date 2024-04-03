34d7c4ee41fe01d95a592a607ab21825fa9f0ca5
Isaac Thapelo Molosiwa <0728921928p@gmail.com>
2024-04-03 13:23:47 +0200

    Update App.jsx

--- Final Capstone DWA/src/App.jsx
+++ Final Capstone DWA/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import SigninForm from "../components/Login Form/Form";
 import Middle from "../components/Context/Middle";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import '@shoelace-style/shoelace/dist/themes/light.css';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
